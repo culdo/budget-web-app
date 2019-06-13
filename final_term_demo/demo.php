7806583db62068af1901962afaecbe391eaef992
Chun Hu <cbc106008@stmail.nptu.edu.tw>
2019-06-13 23:57:45 +0800

    add delete button

--- final_term_demo/demo.php
+++ final_term_demo/demo.php
@@ -52,7 +52,7 @@
                             if($param=="date") {
                                 $instr .= " like '$value%'";
                             } else {
-                                $instr .= " = '$value'";
+                                $instr .= " like '%$value%'";
                             }
 
                             if($is_first_q == true) {
@@ -60,20 +60,20 @@
                             }
                         }
                     }
-                    $instr .= " order by date desc";
+                    $instr .= " order by date desc, id desc";
                     
                 } elseif($op_type=="insert") {
-                    $query = array();
-                    foreach ($_GET as $param=>$value) {
-                        if($param!="op_type" and $value!=null) {
-                            $param = $conn->escape_string($param);
-                            $value = $conn->escape_string($value);
-                            $query[$param] = "'$value'";
+                    if(!empty($_GET["cost"])) {
+                        $query = array();
+                        foreach ($_GET as $param=>$value) {
+                            if($param!="op_type" and $value!=null) {
+                                $param = $conn->escape_string($param);
+                                $value = $conn->escape_string($value);
+                                $query[$param] = "'$value'";
+                            }
                         }
-                    }
-                    $ps = implode(",", array_keys($query));
-                    $vs = implode(",", array_values($query));
-                    if(!empty($query["cost"])) {
+                        $ps = implode(",", array_keys($query));
+                        $vs = implode(",", array_values($query));
                         $instr = "insert into lose_money ($ps) values ($vs)";
                     } else {
                         echo "你沒噴錢新增三小喇。";
@@ -90,22 +90,26 @@
                         $kind = $meals[$kind];
                         $instr = "select * from lose_money where kind in ('$kind') order by rand() limit 1 ";
                     }
-                }elseif($op_type=="delete") {
-                    
+                }elseif($op_type=="delete" and !empty($_GET["id"])) {
+                    $del_id = $conn->escape_string($_GET["id"]);
+                    $instr = "delete from lose_money where id=$del_id";
                 } else {
-                    echo "我Get不到你想做啥";
+                    echo "我Get不到你想做啥就丟全部";
+                    $instr="select * from lose_money order by date desc, id desc";
                 }
                 
                 if($instr!=null) {
                     // echo $instr."<br>";
                     $result=$conn->query($instr);
                     
-                    $is_inserted = false;
                     if($result===true) {
-                        $last_id = $conn->insert_id;
-                        $result=$conn->query("select * from lose_money where id='$last_id' order by date desc");
-                        $is_inserted = true;
-                    } 
+                        if($op_type=="insert" or $op_type=="delete") {
+                            if($op_type=="insert") {
+                                $last_id = $conn->insert_id;
+                            }
+                            $result=$conn->query("select * from lose_money order by date desc, id desc");
+                        }
+                    }
                     if ($result->num_rows > 0) {
                         // output data of each row
                         while($row = $result->fetch_assoc()) {
@@ -117,19 +121,28 @@
                             $id = $row["id"];
                             $weekday = '星期' . ['日', '一', '二', '三', '四', '五', '六'][date("w", strtotime($date))];
                             
+                            if($op_type=="delete" and $id==$_GET["next_id"]) {
+                                echo "<h2 id='deleted' style='text-align: center;'>成功刪除此花費<a href='#$del_id'>#$del_id</a></h2>";
+                            }
+
                             echo "<div id='$id' class='card'>
                                     <div class='container'><div>";
-                            if($is_inserted) {
-                                echo "<span style='color:red; border: 2px red solid;'><b>新加入</b></span>";
+                            if($op_type=="insert" and $id==$last_id) {
+                                echo "<span style='color:red; border: 2px red solid;margin-right:5px;'><b>新加入</b></span>";
                             }
-                            echo "<button class='btn-x'>🞩</button></div><h3><b id='ed-name' contenteditable=true>$name</b><span style='float:right;'>噴<span id='ed-cost' contenteditable=true>$cost</span>元</span></h3>
+                            echo "<a href='#$id'>#$id</a><button class='btn-x' onclick='drop_item(this, $id);'>🞩</button>
+                            </div>
+                            <h3>
+                                <b id='ed-name' contenteditable=true>$name</b>
+                                <span style='float:right;'>噴<span id='ed-cost' contenteditable=true>$cost</span>元</span>
+                            </h3>
                                     <p style='white-space:pre;'><span id='ed-kind' contenteditable=true>$kind</span><span>  <span id='ed-info' contenteditable=true>$info</span><span><span style='float:right;'><span id='ed-date' contenteditable=true>$date</span><span>  $weekday</span></p> 
                                 </div>";
                             if($row["image"]!=null) {
                                 $image = $row["image"];
                                 echo "<img src='$image' alt='Avatar' style='width:100%'>";
                             }
-                            echo "</div><br>";
+                            echo "</div>";
                         }
                     } 
                 } 
@@ -140,18 +153,29 @@
     </div>
 
     <script>
+        <?php
+            if(!empty($last_id)) {
+                echo "window.onload = function(){location.href='#$last_id';}\n";
+            }
+        ?>
         function check_form() {
-
+        }
+        function drop_item(bt, id) {
+            let next_el = bt.parentNode.parentNode.parentNode.nextElementSibling;
+            if(next_el.id=="deleted") {
+                next_el = next_el.nextElementSibling;
+            }
+            let next_id = next_el.id;
+            location.href=`demo.php?op_type=delete&id=${id}&next_id=${next_id}#deleted`;
         }
     </script>
     <style>
         .card {
-            position:relative;
             box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
             transition: 0.3s;
-            width: 40%;
+            width: 420px;
             border-radius: 5px;
-            margin: auto;
+            margin: 18px auto;
             background-color: beige;
         }
 
@@ -164,6 +188,7 @@
         }
 
         .container {
+            position:relative;
             padding: 2px 16px;
         }
         input[type="date"]::-webkit-clear-button {
@@ -191,11 +216,13 @@
             border-bottom: 1px grey solid;
         }
         .btn-x {
-            background-color:#d9534f;
-            color:white;
+            float:right;
             position:relative;
-            right:-430px;
-            
+            right:-10px;
+            background-color:#d9534f;
+            color:white;    
+            margin:5px 0;
+
             border: none;
             width: 30px;
             height: 30px;
@@ -203,6 +230,14 @@
             border-radius: 15px;
             text-align: center;
             font-size: 12px;
+            box-shadow: 2px 0 4px 0 rgba(0,0,0,0.2);
+            transition: 0.3s;
+        }
+        .btn-x:hover {
+            box-shadow: 4px 0 8px 0 rgba(0,0,0,0.2);
+        }
+        h3 {
+            clear: both;
         }
     </style>
 
